f39647b84f029234d7784ba04bdeb1b931ebbeca
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-07-03 17:10:16 +0100

    add basic Data service

---
+++ src/data/index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+require('angular')
+  .module('app.data', ['$q'])
+  .provider('Data', Data)
+
+function Data () {
+  let data = {
+    books: () => require('./books.json'),
+    book: (id) => require('./books.json').find((book) => book.id === id),
+    authors: () => require('./authors.json'),
+    author: (id) => require('./authors.json').find(author => author.id === id)
+  }
+
+  this.$get = function ($q) {
+    let beforeHook = (param) => $q.when({value: param, completed: false})
+
+    let mockHttp = {
+      get: (url) => {
+        let {endpoint, id} = url.split('/')
+        let res = data[endpoint](id)
+        return res
+          ? $q.when({ code: 200, data: res })
+          : $q.reject({ code: 404, data: { message: 'Not found' } })
+      }
+    }
+
+    return {
+      setBeforeHook: (hook) => { beforeHook = hook },
+      request: function (param) {
+        return beforeHook(param).then(function (hookRes) {
+          if (hookRes.completed) {
+            return hookRes.err
+              ? $q.reject(hookRes.err)
+              : $q.when(hookRes.value)
+          } else {
+            return mockHttp.get(param.url)
+          }
+        })
+      }
+    }
+  }
+}
